0c78073d234b5787def44e7da31eeeed6c4c89c2
pradipinexture <chavdapradip48@gmail.com>
2022-11-15 11:39:30 +0530

    two application communication implemented using keycloak

---
+++ TwoApplicationCommunication/keycloak-demo-2/src/main/java/com/pradip/keycloakdemo2/controller/FrontendController.java
@@ -0,0 +1,23 @@
+package com.pradip.keycloakdemo2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FrontendController {
+
+    @RequestMapping
+    public String gePublicPage(){
+        return "home";
+    }
+
+    @RequestMapping("/user")
+    public String getHomePage(){
+        return "user";
+    }
+
+    @RequestMapping("/admin")
+    public String getAdminPage(){
+        return "admin";
+    }
+}
